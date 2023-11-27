ee28551e37401e058320b00a91af58bab740ed37
RylieWeaver <123048075+RylieWeaver@users.noreply.github.com>
2023-11-26 18:42:12 -0800

    Create cfg-1.sh

---
+++ Project_Excerpts/cfg-1.sh
@@ -0,0 +1,7 @@
+
+source_cfg -v cfg-my-settings.sh
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+# export MODEL_NAME=/software/improve/images/<model>.sif  #Lambda
+# export MODEL_NAME=/lus/grand/projects/CSC249ADOA01/images/<model>.sif  #Polaris
+export PARAM_SET_FILE=hyperparams.json
